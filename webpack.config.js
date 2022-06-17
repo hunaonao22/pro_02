5fff55f06e4ba312c5d4f3c7a5b4cd1a6770f9df
hunaonao <403613391@qq.com>
2022-06-17 20:09:28 +0800

    完成生成html,自动引用js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const { join } = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry:'./src/main.js',
     output:{
-        path:join(__dirname,'/lib'),
+        path:join(__dirname,'lib'),
         filename:'webpack-demo.js',
         clean:true
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:join(__dirname,'/public/index.html')
+        })
+    ]
 }
